c01f4d7fb9e938ca7ba47f3a64c888339adce93a
dynamite-ready <l_r_charles@hotmail.com>
2018-08-10 22:41:09 +0100

    Downgrading to CouchDB 2.1.2

--- install-couchdb.sh
+++ install-couchdb.sh
@@ -7,7 +7,7 @@ sudo apt-get --no-install-recommends -y install \
     build-essential pkg-config runit erlang \
     libicu-dev libmozjs185-dev libcurl4-openssl-dev
 
-wget http://www-eu.apache.org/dist/couchdb/source/2.2.0/apache-couchdb-2.2.0.tar.gz
+wget http://www-eu.apache.org/dist/couchdb/source/2.1.2/apache-couchdb-2.1.2.tar.gz
 
 tar -xvzf apache-couchdb-2.2.0.tar.gz
 cd apache-couchdb-2.2.0/
