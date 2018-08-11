200fcd6625ad2201369767d792b859b23966afab
dynamite-ready <l_r_charles@hotmail.com>
2018-08-11 12:13:04 +0100

    Switching back to 2.2.0

--- install-couchdb.sh
+++ install-couchdb.sh
@@ -7,10 +7,10 @@ sudo apt-get --no-install-recommends -y install \
     build-essential pkg-config runit erlang \
     libicu-dev libmozjs185-dev libcurl4-openssl-dev
 
-wget http://www-eu.apache.org/dist/couchdb/source/2.1.2/apache-couchdb-2.1.2.tar.gz
+wget http://www-eu.apache.org/dist/couchdb/source/2.2.0/apache-couchdb-2.2.0.tar.gz
 
-tar -xvzf apache-couchdb-2.1.2.tar.gz
-cd apache-couchdb-2.1.2/
+tar -xvzf apache-couchdb-2.2.0.tar.gz
+cd apache-couchdb-2.2.0/
 sudo ./configure && make release
 
 sudo adduser --system \
